51cab24ed819d8e874d0fa325e59fcfe521a08cb
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-06-23 22:07:16 -0500

    add and edit working

--- contacts-clean/src/main.jsx
+++ contacts-clean/src/main.jsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import Root from "./routes/root";
+import Root, { loader as rootLoader,   action as rootAction, } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+import EditContact, {action as editAction,} from "./routes/edit";
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,6 +17,26 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
